86b48e10c01b67192cc5d65ae12f47418c8f8261
midv-note <midv@tut.by>
2017-04-17 20:55:16 +0300

    second commit

--- test_program/main.cpp
+++ test_program/main.cpp
@@ -1,10 +1,33 @@
 #include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fstream>
+#include <vector>
+#include <string.h>
+#include <iostream>
+#include <locale>
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    cout << "Hello World!" << endl;
+    ifstream in(argv[1]);
+
+    if (in.is_open()){
+
+        string str;
+        in>>str;
+        in.seekg(0, std::ios::end);
+        str.reserve(in.tellg());
+        in.seekg(0, std::ios::beg);
+
+        str.assign((std::istreambuf_iterator<char>(in)),
+                    std::istreambuf_iterator<char>());
+        cout << str<< endl;
+
+        in.close();
+    }
+
     return 0;
 }
 
